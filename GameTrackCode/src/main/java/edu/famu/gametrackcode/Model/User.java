87b4a928d3b293bb1c45201f05bfdb3bf513e57f
Terrelle Thomas <110430142+TerrelleAThomas@users.noreply.github.com>
2024-02-01 01:23:11 -0500

    1

--- GameTrackCode/src/main/java/edu/famu/gametrackcode/Model/User.java
+++ GameTrackCode/src/main/java/edu/famu/gametrackcode/Model/User.java
@@ -1,4 +1,26 @@
 package edu.famu.gametrackcode.Model;
 
+import jakarta.annotation.Nullable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class User {
+
+    @DocumentId
+
+    private @Nullable String userId;
+    private  String Fullname
+
+
+
+
+
+
 }
